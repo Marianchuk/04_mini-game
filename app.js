69d019598e3b0172d57e37603b9894be8a24e021
Marianchuk <marianchuksv@gmail.com>
2021-11-25 11:06:48 +0200

    finish

---
+++ app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['#f05353', '#e27f46', '#77e246', '#46e2bb', '#46d8e2', '#4697e2']
+const SQUEARES_NUMBER = 700
+
+for (let i = 0; i < SQUEARES_NUMBER; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10 px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
